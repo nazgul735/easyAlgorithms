0523d100c805d19cae090cca8ceaca819d649c6e
Nazgul735 <sebastian.veum@gmail.com>
2022-02-17 14:11:30 +0100

    added subArrayAlgo

---
+++ dayOfTheWeek.py
@@ -0,0 +1,7 @@
+def dayofweek(d, m, y):
+    t = [ 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 ]
+    y -= m < 3
+    print(y)
+    return(( y + int(y / 4) - int(y / 100)+int(y / 400) + t[m - 1] + d) % 7)
+day = dayofweek(30, 8, 2010)
+print(day)
---
+++ subArrayWithEqualAmont.py
@@ -0,0 +1,46 @@
+def maxLen(arr, n):
+
+    hash_map = {} 
+    curr_sum = 0
+    max_len = 0
+    ending_index = -1
+ 
+    for i in range (0, n):
+        if(arr[i] == 0):
+            arr[i] = -1
+        else:
+            arr[i] = 1
+
+    for i in range (0, n):
+
+        curr_sum = curr_sum + arr[i]
+ 
+        if (curr_sum == 0):
+            max_len = i + 1
+            ending_index = i
+ 
+        if curr_sum in hash_map:
+
+            if max_len < i - hash_map[curr_sum]:
+                max_len = i - hash_map[curr_sum]
+                ending_index = i
+        else:
+            hash_map[curr_sum] = i 
+         
+    for i in range (0, n):
+        if(arr[i] == -1):
+            arr[i] = 0
+        else:
+            arr[i] = 1
+             
+    print (ending_index - max_len + 1, end =" ")
+    print ("to", end = " ")
+    print (ending_index)
+ 
+    return max_len
+ 
+# Driver Code
+arr = [1, 0, 0, 1, 0, 1, 1]
+n = len(arr) 
+ 
+maxLen(arr, n)
