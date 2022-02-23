6c1852cb49977d0b29289e72039d3a18fa55e0c1
Felix Veum <felix.veum@gmail.com>
2022-02-23 01:48:37 +0000

    abc

--- LongestValidParentheses.py
+++ LongestValidParentheses.py
@@ -25,16 +25,39 @@ def longestValidParentheses(self, s):
 
 def sortingProperly(self,s):
     l,r=0,0
+    mid=len(s)//2
     for i, j in zip(range(len(s)), range(len(s)-1,-1,-1)):
         l=l+2 if s[i] == '(' and s[j]=='(' else l
         l =l+1 if s[i] == '(' or s[j]=='(' and s[j]!=s[i] else l
         r=r+2 if s[i] == ')' and s[j]==')' else r
         r=r+1 if s[i] == ')' or s[j]==')' and s[j]!=s[i] else r
     l,r=l//2,r//2
-    return int((min(l,r)))*("("+")"), len(int((min(l,r))/2)*("("+")"))*2            
+    return int((min(l,r)))*("("+")"), len(int((min(l,r))/2)*("("+")"))*2     
 
-string="()()()()(()))"
-print("original lengde", len(string))
-print(sortingProperly(0, string))
-print(longestValidParentheses(0, string))
+def binSort(self, s):
+    d={"(":0,")":0}
+    mid=len(s)//2; left=s[:mid]; right=s[mid:]
+    for i, j in zip(range(len(left)), range(len(right))):
+        d["("]+=2 if left[i] == '(' and right[j]=='(' else next
+        d["("]+=1 if left[i] == '(' or right[j]=='(' and right[j]!=left[i] else next
+        d[")"]+=2 if left[i] == ')' and right[j]==')' else next
+        d[")"]+=1 if left[i] == ')' or right[j]==')' and right[j]!=left[i] else next
+    return min(d.values())*("()")     
+
+def patSort(self, s):
+    n = len(s)
+    stack = []
+    longest = [0]*n
+    for i in range(n):
+        if s[i] == "(":
+            stack.append(i)
+        elif stack:
+            start = stack.pop()
+            longest[i] = i - start + 1
+            if start > 0:
+                longest[i] += longest[start - 1]
+    return max(longest or [0]) 
+
+string="()()()"
+print(patSort(0, string))
 
