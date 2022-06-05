f7567edbcfbf023ffa6545db5bd19696defbe647
Nazgul735 <sebastian.veum@gmail.com>
2022-06-06 00:16:22 +0200

    feat:started greedy algo

---
+++ leetCode_java/greedyStocks.java
@@ -0,0 +1,12 @@
+package leetCode_java;
+
+public class greedyStocks {
+    private int[] days;
+    public int[] greedy(int[] stocks){
+        return days;
+
+    }
+    public static void main(String[] args) {
+        int[] stocks={1,2,4,1,7};
+    }
+}
